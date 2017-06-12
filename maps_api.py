34a8cf9f63e486c07fd08e27728dfe34bc558c40
Dmitry Spasskiy <thatguy@yandex-team.ru>
2017-06-12 20:56:27 +0300

    Trying to use Googlw Maps API

--- maps_api.py
+++ maps_api.py
@@ -12,7 +12,7 @@ BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 def get_coordinates(address):
     params = {"address": address, "key": os.environ["MAPS_API"]}
     raw_coordinates = requests.get(BASE_URL, params).json()
-    if raw_coordinates["results"]["status"] == "OK":
+    if raw_coordinates["status"] == "OK":
         coordinates = raw_coordinates["results"][0]["geometry"]["location"]
 
     return coordinates
