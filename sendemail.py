03617f9f89e2397f9402bcadba31e23b9f9b39e2
Dmitry Spasskiy <thatguy@yandex-team.ru>
2017-06-13 23:06:39 +0300

    Add 'feedback' action

--- sendemail.py
+++ sendemail.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals, division, print_function
 
+import os
 import smtplib
 from email.mime.text import MIMEText
 
@@ -9,23 +10,18 @@ from email.mime.text import MIMEText
 def send_email(text):
     # Create message
 
-    me = 'traininginparks@mail.ru'
-    you = 'thatguy@yandex.ru'
-    smtp_server = 'smtp.mail.ru'
-    msg = MIMEText('Message e-mail')
+    me = "thatguy@yandex.ru"
+    you = "thatguy@yandex.ru"
+    msg = MIMEText(text)
     msg["Subject"] = "TrainingInParks Bot Feedback"
-    msg['From'] = me
-    msg['To'] = you
-    s = smtplib.SMTP(smtp_server)
-    s.sendmail(me, [you], msg.as_string())
-    server = smtplib.SMTP("localhost")
-    server.sendmail(msg["From"], msg["To"], msg["Text"])
-    server.quit()
+    msg["From"] = me
+    msg["To"] = you
 
     # Send the message via our own SMTP server, but don't include the envelope header.
 
-    server = smtplib.SMTP("localhost")
+    server = smtplib.SMTP_SSL("smtp.yandex.ru")
     server.sendmail(me, [you], msg.as_string())
+    server.login(me, os.environ["YANDEX_PASSWORD"])
     server.quit()
 
 
