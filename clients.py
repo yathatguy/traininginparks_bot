159e946ac717a0668cd819a62a661311c2065c3d
Dmitry Spasskiy <thatguy@yandex-team.ru>
2017-06-24 18:56:34 +0300

    Add missing module to import

--- clients.py
+++ clients.py
@@ -2,7 +2,6 @@
 
 from __future__ import unicode_literals, division, print_function
 
-import logging
 import os
 
 import pymongo
@@ -20,15 +19,21 @@ if "clients" not in db.collection_names() or db.clients.count() == 0:
 def log_client(bot, update):
     client = {"username": update.message.chat.username}
     client_db = db.clients.find_one({"username": client["username"]})
-    logging.critical(client_db)
     if client_db is None:
         if update.message.chat.first_name:
             client["name"] = update.message.chat.first_name
+        else:
+            client["name"] = None
         if update.message.chat.last_name:
             client["surname"] = update.message.chat.last_name
+        else:
+            update.message.chat.last_name = None
         if update.message.chat.id:
             client["chat_id"] = update.message.chat.id
+        else:
+            client["chat_id"] = None
         db.clients.insert(client)
+
 
 def main():
     pass
