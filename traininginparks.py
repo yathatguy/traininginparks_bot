b817074edd32237f032b56243c3097acd86a8f35
Dmitry Spasskiy <thatguy@yandex-team.ru>
2017-09-04 13:52:11 +0300

    Add logging for SENDALL command

--- traininginparks.py
+++ traininginparks.py
@@ -524,12 +524,10 @@ def sendall(bot, update):
         db = connection["heroku_r261ww1k"]
         client_list = json.loads(json_util.dumps(db["clients"].find({})))
         for client in client_list:
-            logging.info(client)
-            logging.info(type(client))
-            logging.info(client.keys())
+            logging.info(client["username"])
             text = query.message.text[9:]
             try:
-                #                bot.sendMessage(text=text, chat_id=client["chat_id"])
+                bot.sendMessage(text=text, chat_id=client["chat_id"])
                 logging.info("\tMessage sent")
             except Exception as exc:
                 logging.critical("\tMessage WAS NOT sent")
